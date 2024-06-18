72827765234ec7f5bfe54ca8f9de109e3d4c0c83
Ashuvnitender  singh <74336946+arujyo@users.noreply.github.com>
2024-06-18 19:09:25 +0530

    Update infogathering.sh

--- infogathering.sh
+++ infogathering.sh
@@ -43,11 +43,13 @@ echo "Live subdomains saved in: $LIVE_OUTPUT"
 
 #aquatone find all live interesting subdomains for you baby 
 cat  ./subdomain_results/live.txt | aquatone -out aquatone_results
+#################Start Dirsearch ################################
+dirsearch -e conf,config,bak,backup,swp,old,db,sql,asp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip,log,xml,js,json  -l  ./aquatone_results/aquatone_urls.txt   -o  dirsearch_result.out
 ################################# Find all urls using waybackurls  Start ###########################    
 cat ./aquatone_results/aquatone_urls.txt | waybackurls > urls 
 
 ######################auto_gf  Start ###################
 auto_gf urls
 ######################## Nuclei Start ##################
-sudo nuclei -l subdomain_results/all_subdomains.txt -o nuclei.out
+sudo nuclei -l ./subdomain_results/all_subdomains.txt -o nuclei.out
 
