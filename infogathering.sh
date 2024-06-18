c87285113daf6b5ef1070fe1d6265ef88e1978ce
Ashuvnitender  singh <74336946+arujyo@users.noreply.github.com>
2024-06-19 05:26:37 +0530

    Update infogathering.sh

--- infogathering.sh
+++ infogathering.sh
@@ -34,30 +34,30 @@ cat $ASSETFINDER_OUTPUT $SUBFINDER_OUTPUT | sort -u > $FINAL_OUTPUT
 # Check for live subdomains
 echo "[*] Checking for live subdomains..."
 cat $FINAL_OUTPUT | httprobe > $LIVE_OUTPUT
-# Clean up individual tool outputs if needed
+echo "# Clean up individual tool outputs if needed"
 rm $ASSETFINDER_OUTPUT $SUBFINDER_OUTPUT
 
 echo "[*] Subdomain enumeration, live subdomain check, and parameter extraction completed."
 echo "All subdomains saved in: $FINAL_OUTPUT"
 echo "Live subdomains saved in: $LIVE_OUTPUT"
 
-#aquatone find all live interesting subdomains for you baby 
+echo "#aquatone find all live interesting subdomains for you baby "
 cat  ./subdomain_results/live.txt | aquatone -out aquatone_results
-#################Start Dirsearch ################################
+echo "#################Start Dirsearch ################################"
 dirsearch -i 200 -e conf,config,bak,backup,swp,old,db,sql,asp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip,log,xml,js,json  -l  ./aquatone_results/aquatone_urls.txt   -o  dirsearch_result.out
  
-################################# Find all urls using waybackurls  Start ###########################    
+echo "################################# Find all urls using waybackurls  Start ###########################"
 cat ./aquatone_results/aquatone_urls.txt | waybackurls > urls 
 
-######################auto_gf  Start ###################
+echo "######################auto_gf  Start ###################"
 auto_gf urls
-#ffuf lfi finding#############
+echo "#ffuf lfi finding#############"
 ffuf -w /usr/share/seclists/Fuzzing/LFI/LFI-linux-and-windows_by-1N3@CrowdShield.txt:FUZZ -w ./paramiters/lfi.txt:URL -u URLFUZZ -mc 200 -of csv -o lfi-result.txt
-#ffuf sqli Start##############
+echo"#ffuf sqli Start##############"
  ffuf -w /usr/share/seclists/Fuzzing/SQLi/quick-SQLi.txt:FUZZ -w ./paramiters/sqli.txt:URL -u URLFUZZ -mc 200 -of csv -o sqli-result.txt
-#dalfox xss Start ############
+echo "#dalfox xss Start ############"
  sudo dalfox file paramiters/xss.txt
 
-######################## Nuclei Start ##################
+echo "######################## Nuclei Start ##################"
 sudo nuclei -l ./subdomain_results/all_subdomains.txt -o nuclei.out
 
